189aaa4c7f589faded1290ae688396d804c76c9b
Atanu Mondal <atanumandal45@gmail.com>
2022-05-28 12:30:15 +0530

    Fixing the menu btn bug

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -22,16 +22,37 @@ const NavBar = () => {
         ))}
       </ul>
 
-      <div className="app__navbar-menu">
+      {/* <div className="app__navbar-menu">
     
-        {!toggle && <HiMenuAlt4 className="burgger__menu"  onClick={() => setToggle(true)} />}
-
+        {!toggle && <HiMenuAlt4  onClick={() => setToggle(true)} />}
         {toggle && (
           <motion.div
             whileInView={{ x: [300, 0] }}
             transition={{ duration: 0.85, ease: 'easeOut' }}
           >
             <HiX className="cross__btn" onClick={() => setToggle(false)} />
+            <ul>
+              {['home', 'about','project', 'skills', 'work', 'contact'].map((item) => (
+                <li key={item}>
+                  <a href={`#${item}`} onClick={() => setToggle(false)}>
+                    {item}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </motion.div>
+        )}
+      </div> */}
+
+<div className="app__navbar-menu">
+        <HiMenuAlt4 onClick={() => setToggle(true)} />
+
+        {toggle && (
+          <motion.div
+            whileInView={{ x: [300, 0] }}
+            transition={{ duration: 0.85, ease: 'easeOut' }}
+          >
+            <HiX onClick={() => setToggle(false)} />
             <ul>
               {['home', 'about','project', 'skills', 'work', 'contact'].map((item) => (
                 <li key={item}>
